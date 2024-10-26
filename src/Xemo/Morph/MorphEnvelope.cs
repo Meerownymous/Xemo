655e5131e7f24b4799c6cc676fe63cfcdec14968
Meerow <meerow@mirmx.de>
2024-10-26 14:16:04 +0200

    fix: make morphenvelope abstract

--- src/Xemo/Morph/MorphEnvelope.cs
+++ src/Xemo/Morph/MorphEnvelope.cs
@@ -4,7 +4,7 @@ namespace Xemo.Morph;
 /// Envelope for morphs.
 /// </summary>
 /// <param name="morph"></param>
-public sealed class MorphEnvelope<TContent, TShape>(IMorph<TContent,TShape> morph) : 
+public abstract class MorphEnvelope<TContent, TShape>(IMorph<TContent,TShape> morph) : 
     IMorph<TContent, TShape>
 {
     public ValueTask<TShape> Shaped(TContent content) => morph.Shaped(content);
